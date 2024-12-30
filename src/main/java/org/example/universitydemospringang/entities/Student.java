6cb95ce5f1338a7b5b19dbf92a8d2463904aab9a
hamza omaddi <hamza.omaddi@gmail.com>
2024-12-30 17:04:46 +0100

    modify class student

--- src/main/java/org/example/universitydemospringang/entities/Student.java
+++ src/main/java/org/example/universitydemospringang/entities/Student.java
@@ -3,15 +3,14 @@ package org.example.universitydemospringang.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter @Setter
 @Builder
 public class Student {
     @Id
@@ -22,4 +21,24 @@ public class Student {
     private String code;
     private String programId;
     private String photo;
+
+    public static String generateUniqueCode() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
