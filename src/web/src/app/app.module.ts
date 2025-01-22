9ba2da899b0d10a1aa77d3c05bd09113683619c5
hamza omaddi <hamza.omaddi@gmail.com>
2025-01-23 00:09:37 +0100

    modified app module

--- src/web/src/app/app.module.ts
+++ src/web/src/app/app.module.ts
@@ -25,6 +25,10 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import { StudentDetailsComponent } from './component/student-details/student-details.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +42,8 @@ import {HttpClientModule} from "@angular/common/http";
     DashboardComponent,
     StudentsComponent,
     PaymentsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -54,7 +59,10 @@ import {HttpClientModule} from "@angular/common/http";
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
