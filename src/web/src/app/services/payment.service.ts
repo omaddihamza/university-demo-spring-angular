68c06356a334e73a4ce664075ad2abb97afccaa9
hamza omaddi <hamza.omaddi@gmail.com>
2025-01-24 19:21:49 +0100

    added methods paymentsByStudentList and gePaymentFile

--- src/web/src/app/services/payment.service.ts
+++ src/web/src/app/services/payment.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment.development";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
-
+  url= environment.backendHost;
   constructor(private http:HttpClient) { }
 
   paymentList():Observable<any>{
-    return this.http.get("http://localhost:8080/api/payments")
+    return this.http.get(this.url+"payments")
+  }
+  paymentsByStudentList(code:any):Observable<any>{
+    return this.http.get(`${this.url}students/${code}/payment`)
+  }
+  gePaymentFile(id :any):Observable<any>{
+    return this.http.get(`${this.url}paymentFile/${id}`, { responseType: 'blob' })
   }
 }
