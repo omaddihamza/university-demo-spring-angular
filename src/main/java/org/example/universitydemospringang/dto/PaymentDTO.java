8af2528d0bd98dc5a925a96eeecfffe5f2597630
hamza omaddi <hamza.omaddi@gmail.com>
2025-02-02 23:56:54 +0100

    modefie dto payment

--- src/main/java/org/example/universitydemospringang/dto/PaymentDTO.java
+++ src/main/java/org/example/universitydemospringang/dto/PaymentDTO.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.example.universitydemospringang.enumeration.PaymentStatus;
 import org.example.universitydemospringang.enumeration.PaymetType;
 
 import java.time.LocalDate;
@@ -14,11 +13,8 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class PaymentDTO {
-    private Long id;
     private LocalDate date;
     private Double amount;
     private PaymetType type;
-    private PaymentStatus status;
-    private String file;
     private String studentCode;
 }
